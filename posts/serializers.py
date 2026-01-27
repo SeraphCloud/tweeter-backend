00f5b7fcdd8f7c928f30f7fb92658ade1ad569fb
Devv-ictor <dev.victor.alexandre@gmail.com>
2026-01-26 23:11:18 -0300

    feat: adiciona campo is_liked no serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -3,12 +3,19 @@ from .models import Post, Comment
 
 class PostSerializer(serializers.ModelSerializer):
   likes_count = serializers.IntegerField(read_only=True)
+  is_liked = serializers.SerializerMethodField()
   comments_count = serializers.IntegerField(read_only=True)
 
   class Meta:
     model = Post
     fields = ["id", "author", "content", "created_at", "likes_count", "comments_count"]
     read_only_fields = ["id", "author", "created_at", "likes_count", "comments_count"]
+    
+  def get_is_liked(self, obj):
+    user = self.context['request'].user
+    if not user.is_authenticated:
+      return False
+    return obj.likes.filter(user=user).exists()
     
 
 class CommentSerializer(serializers.ModelSerializer):
