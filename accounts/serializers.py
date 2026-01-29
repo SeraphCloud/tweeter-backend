b15e58126c52190531bddac1d0162190e6477aa3
Devv-ictor <dev.victor.alexandre@gmail.com>
2026-01-29 00:30:03 -0300

    fix: adiciona o campo following_ids ao serializer de accounts

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,11 +7,15 @@ User = get_user_model()
 class ProfileSerializer(serializers.ModelSerializer):
   username = serializers.CharField(source='user.username', read_only=True)
   avatar = serializers.ImageField(required=False, allow_null=True)
+  following_ids = serializers.SerializerMethodField()
 
   class Meta:
     model = Profile
-    fields = ["id", "username", "display_name", "avatar"]
+    fields = ["id", "username", "display_name", "avatar", "following_ids"]
     read_only_fields = ["id", "username"]
+
+  def get_following_ids(self, obj):
+    return Follow.objects.filter(follower=obj.user).values_list('following_id', flat=True)
 
 class RegisterSerializer(serializers.ModelSerializer):
   password = serializers.CharField(write_only=True)
